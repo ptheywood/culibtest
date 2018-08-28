0fdeefef5232a6b74494c30a3ca59ec0b2a001e3
Peter Heywood <peethwd@gmail.com>
2018-08-28 11:42:51 +0100

    Correctly get a device function pointer on the host...
    This is a little grim, and will result in very poor use of registers (ie all of the spilling)

--- examples/helloWorld/src/main.cu
+++ examples/helloWorld/src/main.cu
@@ -2,13 +2,17 @@
 
 #include <culibtest.h>
 
-__device__ unsigned int(*someDeviceFunction_ptr)(unsigned int);
+typedef unsigned int(*dfuncptr)(unsigned int);
+
+
 __device__ unsigned int someDeviceFunction(unsigned int N) {
 	printf("someDeviceFunction\n");
 	//unsigned int idx = threadIdx.x + blockDim.x * blockIdx.x;
 	//return idx;
 	return 1;
 }
+
+__device__ dfuncptr someDeviceFunction_ptr = someDeviceFunction;
 
 
 
@@ -21,9 +25,19 @@ int main(int argc, char * argv[]){
 	obj.setPrivateInt(12);
 	printf("privateInt %u\n", obj.getPrivateInt());
 
-	someDeviceFunction_ptr = &someDeviceFunction;
-	printf("%p\n", someDeviceFunction_ptr);
-	unsigned int sum = obj.launchRandomKernal(someDeviceFunction_ptr, 1024);
+
+
+	//someDeviceFunction_ptr = &someDeviceFunction; // bad
+	dfuncptr h_someDeviceFunction_ptr;
+	cudaError_t status = cudaSuccess;
+	status = cudaMemcpyFromSymbol(&h_someDeviceFunction_ptr, someDeviceFunction_ptr, sizeof(dfuncptr));
+	if (status != cudaSuccess) {
+		printf("Error, could not get devidce pointer.\n");
+		return 1;
+	}
+
+	printf("%p, %p\n", someDeviceFunction_ptr, h_someDeviceFunction_ptr);
+	unsigned int sum = obj.launchRandomKernal(h_someDeviceFunction_ptr, 1024);
 
 	printf("sum: %u\n", sum);
 	
